8b19e3d5f4adb4d15d56775de4a1d9a8db5691ef
= <wangzezhong@Zezhongs-MacBook-Pro.local>
2015-11-05 11:31:27 +0800

    install laravel 5.1 porject-flyers

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+
+	<div class="jumbotron">
+		<h1>Project Flyer</h1>
+
+		<p>
+			Use this document as a way to quickly start any new project.All you get is this text and a mostly barebones HTML document.
+		</p>
+
+		<a href="/flyers/create" class="btn btn-primary">Create a Flyer</a>
+
+	</div>
+
+@stop
